65d3439fa5fb315ca142ef3e53aefb2c48805e11
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-13 10:28:59 +0530

    Added Program to Check whether a Char is Vowel or Consonant

---
+++ VowelsConsonants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class VowelsConsonants
+    {          
+       public static void checkVowels()
+        {
+            char ch = Convert.ToChar(Console.ReadLine());
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                Console.WriteLine($"{ch} is a vowel");
+            }
+            else if(ch >= 'a' && ch <= 'z')
+            {
+                Console.WriteLine($"{ch} is a consonant");
+            }
+        }
+    }
+}
