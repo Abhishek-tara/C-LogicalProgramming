888e6781e2ec8844bc4f79fb889c056926214804
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-13 10:20:45 +0530

    Added Program to Check Even or odd number

---
+++ EvenAndOdd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class EvenAndOdd
+    {
+        public static void checkEvenAndOdd() {
+            int num = Convert.ToInt32(Console.ReadLine());
+            if(num % 2 == 0 )
+            {
+                Console.WriteLine($"{num} is a Even number");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is a Odd number");
+            }
+        }
+    }
+}
