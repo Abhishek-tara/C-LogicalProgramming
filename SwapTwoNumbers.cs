7f817ea389ac34ca25d8de9b0869a82dff505f1a
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-13 10:14:51 +0530

    Added Program to Swap two numbers

---
+++ SwapTwoNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class SwapTwoNumbers
+    {
+        public static void printSwapTwoNumbers() {
+            int num1 = 8;
+            int num2 = 6;
+            Console.WriteLine($"Before swapping num1: {num1}, num2: {num2}");
+
+            num1 = num1 * num2;
+            num2 = num1 / num2;
+            num1 = num1 / num2;
+            Console.WriteLine($"After swapping num1: {num1}, num2: {num2}");
+
+
+        }
+    }
+}
