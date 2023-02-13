d224903d1f24b3f2c39859adf6566ffa67e3507c
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-13 10:13:11 +0530

    Added Program to compute Quotient and Remainder

---
+++ QuotientAndRemainder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class QuotientAndRemainder
+    {
+        public static void FindQuotientAndRemainder()
+        {
+            int divident = 14;
+            int divisor = 5;
+            int quotient = divident / divisor;
+            int remainder = divident % divisor;
+
+            Console.WriteLine("Divident: {0}, Divisor: {1} : ", divident,divisor);
+            Console.WriteLine("Quoitent: " + quotient);
+            Console.WriteLine("Remainder: " + remainder);
+        }
+    }
+}
